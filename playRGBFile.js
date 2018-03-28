e1ce45d4fce0069185ae869dffa02f062abe59a5
Luke Chen <doomer@yahoo.com>
2018-03-28 16:44:32 +0800

    remove idle check

--- playRGBFile.js
+++ playRGBFile.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 var player = require('./source/player');
-var idle   = require('./source/idle');
 var fs     = require('fs');
 
 var rgbFile;
@@ -15,22 +14,9 @@ if (process.argv.length < 3) {
 
 var commands = JSON.parse(fs.readFileSync(rgbFile));
 
-player.writeCommandToKeyboard('active');
-
-var refreshRate = 1000/24;
 var index = 0;
-var isIdle = false;
 setInterval(function () {
     index %= commands.length;
-    if (isIdle)
-        player.writeCommandToKeyboard('rgb 000000');
-    else
-        player.writeCommandToKeyboard(commands[index]);
+    player.writeCommandToKeyboard(commands[index]);
     index += 1;
-}, refreshRate);
-setInterval(function() {
-    if (idle.getIdleTime() > 60000)
-        isIdle = true;
-    else
-        isIdle = false;
-}, 4000);
+}, 1000/24);
